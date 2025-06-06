e245d61639353db5404d908d78f356430d7dbd2c
hieuvnd-amcolab <hieuvnd@amcolab.vn>
2025-06-06 10:02:57 +0700

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:audio_toolkit/audio_toolkit.dart';
+import 'package:audio_toolkit/language_type.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:audio_toolkit_example/cubit/audio_toolkit_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -175,4 +180,180 @@ class AudioToolkitScreen extends StatelessWidget {
       ],
     );
   }
+}
+
+class AudioToolkitCubit extends Cubit<AudioToolkitState> {
+  final audioToolkit = AudioToolkit.instance;
+  StreamSubscription? _dbSub;
+  StreamSubscription? _sentenceSub;
+
+  AudioToolkitCubit()
+      : super(AudioToolkitInitial(
+            db: 0.0,
+            isRecording: false,
+            text: [],
+            path: '',
+            isSystemRecord: true));
+
+  Future<void> init() async {
+    await audioToolkit.init();
+    _dbSub = audioToolkit.onDbAudio.listen((db) {
+      final current = state;
+      if (current is AudioToolkitInitial) {
+        _dbSub = audioToolkit.onDbAudio.listen((db) {
+          final current = state;
+          if (current is AudioToolkitInitial) {
+            emit(current.copyWith(db: db, isSystemRecord: true));
+          }
+        });
+      }
+    });
+  }
+
+  Future<void> turnOffSystemRecording() async {
+    final res = await audioToolkit.turnOffSystemRecording();
+    if (res.result) {
+      final current = state;
+      if (current is AudioToolkitInitial) {
+        emit(current.copyWith(db: -160, isSystemRecord: false));
+        _dbSub?.cancel();
+      }
+    }
+  }
+
+  Future<void> turnOnSystemRecording() async {
+    final res = await audioToolkit.turnOnSystemRecording();
+    if (res.result) {
+      _dbSub = audioToolkit.onDbAudio.listen((db) {
+        final current = state;
+        if (current is AudioToolkitInitial) {
+          emit(current.copyWith(
+            db: db,
+          ));
+        }
+      });
+    }
+  }
+
+  Future<void> startRecord() async {
+    final res = await audioToolkit.startRecord();
+    if (res.result) {
+      final current = state;
+      if (current is AudioToolkitInitial) {
+        emit(current.copyWith(isRecording: true));
+      }
+      _sentenceSub = audioToolkit.onSentenceDetected.listen(
+        (path) async {
+          final current0 = state;
+          if (current0 is AudioToolkitInitial) {
+            emit(current0.copyWith(path: path));
+          }
+        },
+      );
+    }
+  }
+
+  Future<void> stopRecording() async {
+    final res = await audioToolkit.stopRecording();
+    if (res.result) {
+      final current = state;
+      if (current is AudioToolkitInitial) {
+        emit(current.copyWith(isRecording: false));
+      }
+      _sentenceSub?.cancel();
+      deleteRecordingFilesExceptFull();
+    }
+  }
+
+  Future<void> deleteRecordingFilesExceptFull() async {
+    final downloadsDir = Directory('${Platform.environment['HOME']}/Downloads');
+    if (await downloadsDir.exists()) {
+      final files = downloadsDir.listSync();
+
+      for (var file in files) {
+        if (file is File) {
+          final name = file.uri.pathSegments.last;
+
+          if (name.startsWith('audioToolkit_') && !name.endsWith('_full.m4a')) {
+            try {
+              await file.delete();
+            } catch (e) {}
+          }
+        }
+      }
+    }
+  }
+
+  Future<void> transcribeAudio(String path) async {
+    final res = await audioToolkit.transcribeAudio(path, LanguageType.vi);
+    final current = state;
+    if (current is AudioToolkitInitial) {
+      if (res.result) {
+        final text = res.text;
+        if (text != null) {
+          List<String> updatedText = [text, ...current.text];
+          emit(current.copyWith(text: updatedText));
+        }
+      } else {
+        final text = res.errorMessage;
+        if (text != null) {
+          List<String> updatedText = [text, ...current.text];
+          emit(current.copyWith(text: updatedText));
+        }
+      }
+    }
+
+    if (res.path != null) {
+      try {
+        final file = File(res.path ?? '');
+        await file.delete();
+      } catch (e) {}
+      final current = state;
+      if (current is AudioToolkitInitial) {
+        emit(current.copyWith(path: ''));
+      }
+    }
+  }
+}
+
+sealed class AudioToolkitState extends Equatable {
+  const AudioToolkitState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class InitState extends AudioToolkitState {}
+
+final class AudioToolkitInitial extends AudioToolkitState {
+  final List<String> text;
+  final bool isRecording;
+  final bool isSystemRecord;
+  final double db;
+  final String path;
+  const AudioToolkitInitial({
+    required this.isRecording,
+    required this.text,
+    required this.db,
+    required this.isSystemRecord,
+    required this.path,
+  });
+
+  AudioToolkitInitial copyWith({
+    bool? isRecording,
+    bool? isSystemRecord,
+    double? db,
+    List<String>? text,
+    String? path,
+  }) {
+    return AudioToolkitInitial(
+        text: text ?? this.text,
+        isSystemRecord: isSystemRecord ?? this.isSystemRecord,
+        isRecording: isRecording ?? this.isRecording,
+        db: db ?? this.db,
+        path: path ?? this.path);
+  }
+
+  @override
+  List<Object> get props => [isRecording, db, text, path];
 }
