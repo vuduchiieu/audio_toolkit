8902cfeef7eb903627404374d2ecb4f8d5881fb9
hieuvnd-amcolab <hieuvnd@amcolab.vn>
2025-09-19 14:40:55 +0700

    feat: respon live

--- lib/audio_toolkit_method_channel.dart
+++ lib/audio_toolkit_method_channel.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 import 'audio_toolkit_platform_interface.dart';
 
 class MethodChannelAudioToolkit extends AudioToolkitPlatform {
-  @visibleForTesting
   final methodChannel = const MethodChannel('audio_toolkit');
 
   @override
